874670c1bb291bb5e01b226aa0556df2329f94a3
Doug Currie <>
2018-06-25 21:13:40 -0400

    Add Hamming-weight dependency test.

---
+++ hwd/prngs_hwd.c
@@ -0,0 +1,49 @@
+
+#if 1
+
+#include "../xorshiftstare.h"
+
+#if HWD_PRNG_BITS == 64
+
+static uint128_v rng_state = 1u;
+
+static inline uint64_t next (void)
+{
+    return RNG_NAME(&rng_state);
+}
+
+#else
+
+static uint64_t rng_state = 1u;
+
+static inline uint32_t next (void)
+{
+    return RNG_NAME(&rng_state);
+}
+
+#endif
+
+#else
+
+/* Sample file for hwd.c (xoroshiro128+). */
+
+static inline uint64_t rotl(const uint64_t x, int k) {
+    return (x << k) | (x >> (64 - k));
+}
+
+static uint64_t s[2] = { 1, -1ULL };
+
+static uint64_t inline next() {
+    const uint64_t s0 = s[0];
+    uint64_t s1 = s[1];
+
+    const uint64_t result_plus = s0 + s1;
+
+    s1 ^= s0;
+    s[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16);
+    s[1] = rotl(s1, 37);
+
+    return result_plus;
+}
+
+#endif
